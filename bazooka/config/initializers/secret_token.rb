b68db26afd1af044bf77cfc74bf4d2d8b190dbe6
pavan <pavan.akella@chronus.com>
2014-08-11 12:31:08 +0530

    first commit

---
+++ bazooka/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazooka::Application.config.secret_token = '987e1ad2aa2bafb15e5f266a3522c79f8b186115c892ab0b650a2675b81a78bcc6059e92223fa850f5927229edd8c5daf776dfedae85c7c96f7855e8053a24e2'
